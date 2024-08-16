a6c4e8edb2c8929ba032fe37ce7477191b981bc4
Anh Vu <tuananhvutran132@gmail.com>
2024-08-15 19:36:07 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,15 +7,25 @@ const serviceRoutes = require("./routes/service")
 const { app } = require('./models/service')
 
 connection()
-//mongoose.connect("mongodb+srv://anhvu:Supersentai3@cluster0.nmvm044.mongodb.net/salon")
 
 // middlewares
 app.use(express.json());
 
-app.use("/", serviceRoutes)
+app.use("/main", serviceRoutes)
 app.get('/api', (req, res) => {
     res.send('Hello from Express!');
 }); 
+app.get("/", async (req, res) => {
+    try {
+        const services = await Service.find({});
+        res.status(200).json(services);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Internal server error" });
+    }
+});
+
+
 const port = process.env.PORT || 3001
 app.listen(port, () => {
     console.log("Server is running!!!");
